7f64126834585e1aab9d5763f32e205d2029f1ff
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-12-17 20:59:22 +0300

    fix window size

--- conftest.py
+++ conftest.py
@@ -8,8 +8,8 @@ from utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    browser.config.window_width = 1600
-    browser.config.window_height = 900
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
     options = Options()
     selenoid_capabilities = {
