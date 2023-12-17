99da747d07b418143b3b53e65a3ce326737b9f18
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-12-17 19:49:05 +0300

    fix for conftest

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
-    capabilities = {
+    options = Options()
+    selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
@@ -13,10 +15,11 @@ def setup_browser(request):
             "enableVideo": False
         }
     }
-
+    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor="http://user1:1234@selenoid:4444/wd/hub",
-        desired_capabilities=capabilities)
+        desired_capabilities=selenoid_capabilities)
 
+    browser.config.driver = driver
     yield
     browser.quit()
